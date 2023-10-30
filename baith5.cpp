d58d9520019fd7866385fd352ba923437044f60e
thien08102005 <147484520+thien08102005@users.noreply.github.com>
2023-10-30 09:52:36 +0700

    Add files via upload

---
+++ baith5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    char name[20];
+    printf("Ten cua ban la : ");
+    scanf("%s", name);
+    printf("ten cua ban %s.", name);
+    return 0;
+}
